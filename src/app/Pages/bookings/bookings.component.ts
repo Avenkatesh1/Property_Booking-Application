9608c93ef0e6673234372121396536808004fd27
A Venkatesh <avenkateshanand1998@gmail.com>
2024-09-21 02:21:18 +0530

    i have committed and Submited the Form

--- src/app/Pages/bookings/bookings.component.ts
+++ src/app/Pages/bookings/bookings.component.ts
@@ -3,12 +3,12 @@ import { map, Observable } from 'rxjs';
 import { IAPIResponseModel, IProperty, Site } from '../../model/master';
 import { MasterService } from '../../Service/master.service';
 import { AsyncPipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bookings',
   standalone: true,
-  imports: [AsyncPipe, FormsModule],
+  imports: [AsyncPipe, FormsModule, ReactiveFormsModule],
   templateUrl: './bookings.component.html',
   styleUrl: './bookings.component.css',
 })
@@ -17,8 +17,13 @@ export class BookingsComponent {
   masterS = inject(MasterService);
   siteId: number = 0;
   propertiesList: IProperty[] = [];
+  bookingList: any[] = [];
+  currentPropertyId:number =0;
+
+  bookingForm: FormGroup = new FormGroup({});
 
   constructor() {
+    this.initializeBookinForm();
     this.sites$ = this.masterS.getAllSites().pipe(
       map((res: IAPIResponseModel) => {
         return res.data;
@@ -26,29 +31,55 @@ export class BookingsComponent {
     );
   }
 
+  initializeBookinForm(){
+    this.bookingForm = new FormGroup({
+       bookingId: new FormControl(0),
+       propertyId: new FormControl(this.currentPropertyId),
+       bookingRate: new FormControl(''),
+       totalAmount: new FormControl(''),
+       custId: new FormControl(0),
+       bookingDate: new FormControl(''),
+       name: new FormControl(''),
+       mobile: new FormControl(''),
+       emailid: new FormControl(''),
+       address: new FormControl(''),
+    }) 
+  }
+
   getProperties(event: Event) {
-    this.masterS
-      .getAllPropertyBySiteId(this.siteId)
-      .subscribe((res: IAPIResponseModel) => {
+    this.getBookingBySiteId()
+    this.masterS.getAllPropertyBySiteId(this.siteId).subscribe((res: IAPIResponseModel) => {
         this.propertiesList = res.data;
+      });
+  }
+  getBookingBySiteId() {
+    this.masterS.getAllPropertyBookingBySiteId(this.siteId).subscribe((res: IAPIResponseModel) => {
+        this.bookingList = res.data;
       });
   }
 
   openModal(data:IProperty)  {
+    this.currentPropertyId = data.propertyId;
+    this.initializeBookinForm()
     const model = document.getElementById('modalPapup');
-
     if (model) {
       model.style.display = 'block';
     }
   }
   closeModal() {
     const model = document.getElementById('modalPapup');
-
     if (model) {
       model.style.display = 'none';
     }
   }
   onPropertiesSave(){
-    
+    this.masterS.onSaveBooking(this.bookingForm.value).subscribe((res: IAPIResponseModel)=>{
+      if(res.result) {
+        alert("Record's Saved Successfully");
+        this.getBookingBySiteId();
+      } else {
+        alert(res.messege)
+      }
+    })
   }
 }
